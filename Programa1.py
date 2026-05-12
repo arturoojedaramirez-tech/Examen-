7e30b0db8f633f92082c4b9f3c4038746ee1addc
arturoojedaramirez-tech <arturoojedaramirez693@gmail.com>
2026-05-12 10:11:30 -0600

    Create Programa1.py

---
+++ Programa1.py
@@ -0,0 +1,10 @@
+Num1 = float(input("Dame n1: "))
+Num2 = float(input("Dame n2: "))
+
+producto = Num1 * Num2
+
+if producto <= 1000:
+    print(producto)
+else:
+    Prod = Num1 + Num2
+    print(Prod)
